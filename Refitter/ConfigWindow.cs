b67a7a1498b3631ef337e54b668a00dab2ca5671
Joshua Goins <josh@redstrate.com>
2024-08-08 22:06:09 -0400

    Rearrange the config window elements considerably

--- Refitter/ConfigWindow.cs
+++ Refitter/ConfigWindow.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Numerics;
 using Dalamud.Interface.Windowing;
 using FFXIVClientStructs.FFXIV.Client.Game.Character;
 using ImGuiNET;
@@ -14,7 +13,7 @@ public class ConfigWindow(Plugin plugin)
     public override void Draw()
     {
         ImGui.Checkbox("Enable Adjustments", ref plugin.EnableOverrides);
-
+        
         unsafe
         {
             var localPlayer = Plugin.ClientState.LocalPlayer;
@@ -24,46 +23,48 @@ public class ConfigWindow(Plugin plugin)
             if (gameObject == null) return;
 
             var torsoData = gameObject->DrawData.Equipment(DrawDataContainer.EquipmentSlot.Body);
-            ImGui.Text($"Current Torso ID: {torsoData.Id}");
-
-            ImGui.Separator();
-
-            ImGui.TextDisabled("Adjustments");
-
-            if (ImGui.Button("Add Override"))
-            {
-                var configModel = new ConfigModel
-                {
-                    Model = torsoData.Id
-                };
-                Plugin.Configuration.Configs.Add(configModel);
-            }
-
-            if (ImGui.Button("Save")) Plugin.Configuration.Save();
 
             var previewSmallClothes = plugin.PreviewSmallclothes;
             if (ImGui.Checkbox("Preview Smallclothes", ref previewSmallClothes))
                 plugin.PreviewSmallclothes = previewSmallClothes;
 
-            ImGui.BeginChild("overrides", new Vector2(-1, -1), true);
-
-            var adjustment = Plugin.Configuration.Configs.Find(x => x.Model == torsoData.Id);
-            if (adjustment != null)
+            if (previewSmallClothes)
             {
-                ImGui.Text("Model Entry");
-
-                ImGui.DragFloat("Gravity", ref adjustment.Gravity, 0.001f);
-                ImGui.DragFloat3("Scale Override", ref adjustment.NewScale, 0.001f);
-                ImGui.DragFloat3("Pos Override", ref adjustment.NewPos, 0.001f);
-                ImGui.DragFloat("Push Down", ref adjustment.PushDown, 0.001f);
-                ImGui.DragFloat("Push Up", ref adjustment.PushUp, 0.001f);
-
                 ImGui.Separator();
+
+                ImGui.TextDisabled("Cannot adjust while previewing smallclothes.");
             }
             else
-                ImGui.TextDisabled("No adjustment for this torso item.");
+            {
+                var adjustment = Plugin.Configuration.Configs.Find(x => x.Model == torsoData.Id);
+                if (adjustment != null)
+                {
+                    ImGui.Separator();
 
-            ImGui.EndChild();
+                    ImGui.TextDisabled($"Adjustments for {torsoData.Id}");
+
+                    ImGui.DragFloat("Gravity", ref adjustment.Gravity, 0.001f);
+                    ImGui.DragFloat3("Scale Override", ref adjustment.NewScale, 0.001f);
+                    ImGui.DragFloat3("Pos Override", ref adjustment.NewPos, 0.001f);
+                    ImGui.DragFloat("Push Down", ref adjustment.PushDown, 0.001f);
+                    ImGui.DragFloat("Push Up", ref adjustment.PushUp, 0.001f);
+
+                    if (ImGui.Button("Save")) Plugin.Configuration.Save();
+                }
+                else
+                {
+                    ImGui.TextDisabled("No adjustment for this torso item.");
+
+                    if (ImGui.Button("Adjust"))
+                    {
+                        var configModel = new ConfigModel
+                        {
+                            Model = torsoData.Id
+                        };
+                        Plugin.Configuration.Configs.Add(configModel);
+                    }
+                }
+            }
         }
     }
 }
